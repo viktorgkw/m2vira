5c93ef516c6d7e4335cc1dacd86003ba770605b5
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-10 18:20:58 +0200

    Permissions route added.

---
+++ src/app/api/permissions/route.ts
@@ -0,0 +1,26 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connect();
+
+    const { email } = await req.json();
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json({
+        status: 404,
+      });
+    }
+
+    return NextResponse.json({
+      status: 200,
+      isAdmin: user.isAdmin,
+    });
+  } catch (err: any) {
+    return NextResponse.json({ message: err.message, status: 500 });
+  }
+}
--- src/app/products/create/page.tsx
+++ src/app/products/create/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 import { Loading } from "@/app/components/Loading";
 import { CreateProductForm } from "@/app/components/Products/CreateProductForm";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 export default function CreateProduct() {
+  const router = useRouter();
+
   const { data: session } = useSession({
     required: true,
     onUnauthenticated() {
@@ -13,9 +16,31 @@ export default function CreateProduct() {
     },
   });
 
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`https://m2vira.vercel.app/api/permissions`, {
+        method: "POST",
+        body: JSON.stringify({ email: session?.user?.email }),
+      });
+
+      const data = await res.json();
+
+      if (!data.isAdmin) {
+        router.push("/products/all");
+        return;
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [router, session]);
+
   return (
     <div className="flex items-center justify-center">
-      {!session?.user ? <Loading /> : <CreateProductForm />}
+      {loading ? <Loading /> : <CreateProductForm />}
     </div>
   );
 }
