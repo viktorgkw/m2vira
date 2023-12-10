89ce9e34a3b5c6b126d6abc052a0f92f7e90fe9f
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-10 15:16:40 +0200

    Testing revalidation in ProductAll

--- src/app/products/all/page.tsx
+++ src/app/products/all/page.tsx
@@ -3,13 +3,11 @@ import { ProductsDisplay } from "@/app/components/Products/ProductsDisplay";
 import { getImage } from "@/helpers/firebaseStorage";
 
 export default async function AllProducts() {
-  const response = await fetch(`https://m2vira.vercel.app/api/products/all`, {
-    cache: "no-store",
-  });
+  const response = await fetch(`https://m2vira.vercel.app/api/products/all`);
 
   const data = await response.json();
 
-  if (data.status !== 200) return <NoProducts />;
+  if (data.status !== 200 || data.products.length === 0) return <NoProducts />;
 
   await Promise.all(
     data.products.map(async (p: any) => {
@@ -23,3 +21,5 @@ export default async function AllProducts() {
     </div>
   );
 }
+
+export const revalidate = 10;
