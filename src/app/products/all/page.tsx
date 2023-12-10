6948cb85015fd1b64831a765801d3a1ea45a5f39
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-10 13:07:47 +0200

    Fixed page AllProducts

--- src/app/products/all/page.tsx
+++ src/app/products/all/page.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NoProducts } from "@/app/components/Products/NoProducts";
 import { ProductsDisplay } from "@/app/components/Products/ProductsDisplay";
 import { getImage } from "@/helpers/firebaseStorage";
 
 export default async function AllProducts() {
-  const response = await fetch(`https://m2vira.vercel.app/api/products/all`, {
-    next: { revalidate: 5 },
-  });
-  const data = await response.json();
+  const [products, setProducts] = useState([]);
 
-  if (data.status !== 200) return <NoProducts />;
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://m2vira.vercel.app/api/products/all`,
+        {
+          next: { revalidate: 5 },
+        }
+      );
+      const data = await response.json();
 
-  await Promise.all(
-    data.products.map(async (p: any) => {
-      p.image = await getImage(p.title);
-    })
-  );
+      if (data.status !== 200) return <NoProducts />;
+
+      await Promise.all(
+        data.products.map(async (p: any) => {
+          p.image = await getImage(p.title);
+        })
+      );
+
+      setProducts(data.products);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <ProductsDisplay products={data.products} />
+      <ProductsDisplay products={products} />
     </div>
   );
 }
