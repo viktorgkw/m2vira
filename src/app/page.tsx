eb2f0add705e707e62913349ecc738eae9d3ce03
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-11 15:42:44 +0200

    Home page updated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,27 @@ export default function Home() {
 
   return (
     <>
+      <section className="">
+        <div className="flex flex-col md:flex-row items-center max-w-6xl mx-auto mt-16 mb-24">
+          <div className="w-full md:w-1/2 py-8">
+            <div className="flex flex-col uppercase text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-blue-800 to-blue-900">
+              <p className="text-4xl md:text-6xl mb-4">Welcome</p>
+              <p className="text-xl md:text-2xl mb-4">to</p>
+              <p className="text-4xl md:text-6xl mb-4">m2vira</p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 py-8">
+            <Image
+              src="/images/welcome.jpg"
+              className="rounded-3xl"
+              width={512}
+              height={512}
+              alt="Logo"
+            />
+          </div>
+        </div>
+      </section>
+
       <div className="flex flex-wrap items-center justify-center my-8">
         <figure className="relative transition-all duration-300 filter md:grayscale md:hover:grayscale-0 m-3">
           <Image
