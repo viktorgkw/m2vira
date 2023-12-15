131d2707baa851358bb830af5c5b58292e6e2ee0
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-15 19:54:24 +0200

    Design updates.

--- src/app/products/details/[id]/page.tsx
+++ src/app/products/details/[id]/page.tsx
@@ -53,10 +53,10 @@ export default async function ProductDetails({ params }: any) {
               <Image
                 key={i}
                 priority
-                width={512}
-                height={512}
+                width={320}
+                height={410}
                 alt="Product Image"
-                className="w-full md:w-[25%] rounded hover:scale-[1.03] duration-300 mt-8"
+                className="w-full md:w-[25%] rounded-xl hover:scale-[1.03] duration-300 mt-8 mx-4"
                 src={img}
               />
             ))}
