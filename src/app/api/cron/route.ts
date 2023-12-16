352e2cd3c2bd0def8a33314a7ff610dad6489c7e
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-16 12:21:01 +0200

    CronJob added.

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/helpers/mongoDB";
+import Promocodes from "@/models/promocodeModel";
+
+export async function GET(request: NextRequest) {
+  try {
+    if (
+      request.headers.get("Authorization") !==
+      `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
+
+    await connect();
+
+    const date = new Date();
+    date.setMonth(date.getMonth() - 1);
+
+    await Promocodes.deleteMany({ createdAt: { $lt: date } });
+
+    return NextResponse.json({
+      message: "(Monthly promocodes cleanup) Cron executed successfully!",
+      status: 200,
+    });
+  } catch (err: any) {
+    return NextResponse.json({
+      message: err.message,
+      status: 500,
+    });
+  }
+}
